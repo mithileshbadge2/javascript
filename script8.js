342c113f4ee0a197c35e6789efebdb2de0da38f7
mithileshbadge2 <mithileshbadge2@gmail.com>
2024-02-28 20:29:26 +0530

    added

---
+++ script8.js
@@ -0,0 +1,47 @@
+let cricket = ["virat","rohit","dhoni",'rahul','kuldeep']
+console.log(cricket)
+console.log(typeof(cricket))
+console.log(cricket[2])
+
+let q1 = cricket.length
+console.log(q1)
+
+//to print all index number
+for(let i = 0; i < 5; i++){
+    console.log(i)
+}
+
+let cities = ['nagpur','pune','hyderabad','indore']
+//console.log(cities[2])
+//console.log(cities.length)
+for(let i = 0 ; i < cities.length ; i++){
+    //console.log(i)
+    console.log(cities[i])
+}
+
+for(let i = cities.length -1 ; i >= 0;i--){
+    //console.log(i)
+    console.log(cities[i])
+}
+
+let games = ['fifa','cod','tekken','ludo']
+console.log(games)
+let y = 0
+while(y < games.length){
+    //console.log(y)
+    console.log(games[y])
+    y = y + 1
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
