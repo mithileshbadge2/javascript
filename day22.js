a6e1b4946e53230dae0c1a9d5b5e539f30273b56
mithileshbadge2 <mithileshbadge2@gmail.com>
2024-06-10 19:47:18 +0530

    added

---
+++ day22.js
@@ -0,0 +1,33 @@
+let firstName = "vedant"
+a = firstName.toUpperCase()
+console.log(a)
+
+let lastName = "BADGE"
+b = lastName.toLowerCase()
+console.log(b)
+
+let city = "Mumbai"
+let c = city.toUpperCase().toLowerCase().length
+console.log(c)
+
+let fruits = "apple mango banana chikoo grapes"
+let d = fruits.includes('m')
+let e = fruits.includes('Mango')
+console.log(d)
+console.log(e)
+
+let area = "gaddigodam"
+f = area.indexOf('g')
+g = area.indexOf('G')
+console.log(f)
+console.log(g)
+
+let city3 = "gaddigodam"
+console.log(city3.slice(0))
+console.log(city3.slice(0,5))
+console.log(city3.slice(-8,-3))
+
+let e1 = "I am learning javascript and playwright" 
+let e2 = e1.replace("javascript","cypress")
+console.log(e2)
+
