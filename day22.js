0591cb9b7128cd77998f5ea8d9509e021ebbb579
mithileshbadge2 <mithileshbadge2@gmail.com>
2024-06-11 20:23:07 +0530

    added

--- day22.js
+++ day22.js
@@ -31,3 +31,57 @@ let e1 = "I am learning javascript and playwright"
 let e2 = e1.replace("javascript","cypress")
 console.log(e2)
 
+let info = "I am learning js"
+let a1 = info.concat(" and python")
+console.log(a1)
+console.log(info)
+
+reverse = ["ramesh","suresh","mangesh","kunal"]
+let r = reverse.reverse()
+console.log(r)
+console.log(reverse)
+
+let nam = ["ramesh","suresh","mangesh","kunal"]
+let sorting = nam.sort()
+console.log(sorting)
+console.log(nam)
+
+let fullName = "Mithilesh Avinash Badge"
+let fn = fullName.split(" ")
+let fn1 = fullName.split("a")
+console.log(fn)
+console.log(fn1)
+
+let shortName = "monty badge"
+let a11 = shortName.startsWith("mon")
+let a12 = shortName.startsWith("bad")
+console.log(a11)
+console.log(a12)
+
+a13 = shortName.endsWith("dge")
+a14 = shortName.endsWith('e')
+console.log(a13)
+console.log(a14)
+
+let cit = " mumbai "
+let a15 = cit.trimStart()
+console.log(a15)
+
+let cit2 = "goa "
+let a16 = cit2.trimEnd()
+console.log(a16)
+
+let cit3 = " indore "
+let a17 = cit3.trim()
+console.log(a17)
+
+let a111 = "monty"
+let a18 = a111.charAt(4)
+console.log(a18)
+
+a19 = a111.charCodeAt(4)
+console.log(a19)
+
+
+
+
