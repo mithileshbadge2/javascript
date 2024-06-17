4e16d56dd613dcf4b2ce8f6220081316ab365283
mithileshbadge2 <mithileshbadge2@gmail.com>
2024-06-17 20:35:49 +0530

    added

---
+++ DOMD2/script.js
@@ -0,0 +1,12 @@
+let para = document.querySelector('p')
+console.log(para)
+
+let byId = document.querySelector('#onee')
+console.log(byId)
+
+let byClassName = document.querySelector(".two")
+console.log(byClassName)
+
+//tagName[attribute= "value"]
+let byAtt = document.querySelector('p[name = "nm"]')
+console.log(byAtt)
