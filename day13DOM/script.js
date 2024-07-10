1c3ba0111d6a94bd79670029fe4d4145c4ba66dc
mithileshbadge2 <mithileshbadge2@gmail.com>
2024-07-10 20:24:24 +0530

    added

---
+++ day13DOM/script.js
@@ -0,0 +1,7 @@
+let Para = document.querySelector('p')
+let buttonA = document.querySelector('button')
+
+buttonA.addEventListener('click',function(){
+    Para.removeAttribute('class')
+    // Para.classList.add('three')
+})
